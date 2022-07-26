cfa8d1fc6f1813e38cf54350b5b7c87c154803bf
Panayiotis Halouvas <phalouvas@kainotomo.com>
2022-07-26 11:09:23 +0000

    added middleware to route

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('cms::welcome');
-})->name('phmoney.cms.welcome');
+})->name('phmoney.cms.welcome')->middleware('web');
